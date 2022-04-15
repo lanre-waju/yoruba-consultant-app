b150c7704b2034f0a02ca0af8def98505ac2fcf7
Victor Edet <edetvictor849@gmail.com>
2022-04-15 10:41:34 +0100

    (PYC)Chore:header and footer layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return(
+        <div className="Footer position-fixed bottom-0 col-8 bg-black py-2" >
+            <p className="text-center text-white m-2">all rights reserved @ lanre</p>
+        </div>
+    )
+}
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return(
+        <div className="Header  d-flex  bg-primary text-white shadow" >
+            <h3 className="col-6 fw-bold m-2">YCA</h3>
+            <div className="col-6 m-2 d-flex text-capitalize justify-content-between">
+                <p className="col">home</p>
+                <p className="col">about</p>
+                <p className="col">get-started</p>
+            </div>
+        </div>
+    )
+}
+export default Header
